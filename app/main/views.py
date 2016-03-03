df76d94d89750d9a5adedd109ae1609ca03f5fe9
liu <1020269358@qq.com>
2016-03-03 11:59:02 +0800

    11.5

--- app/main/views.py
+++ app/main/views.py
@@ -100,3 +100,8 @@ def edit_profile_admin(id):
 	form.about_me.data = user.about_me
 	return render_template('edit_profile.html', form = form, use = user)
 	#和edit_profile类似
+
+@main.route('/post/<int:id>')
+def post(id):
+	post = Post.query.get_or_404(id)
+	return render_template('post.html', posts = [post])
